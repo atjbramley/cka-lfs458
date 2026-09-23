0b48829efd8d3afdb24837d8acb520cf45cc84a5
atjbramley <atjbramley@hotmail.com>
2026-09-23 12:03:53 +0200

    fix ansible inventory with bws managed secrets

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,3 +1,11 @@
 resource "random_id" "suffix" {
   byte_length = 4
 }
+
+# Pass GCP service account key for use in  Ansible's gcp_compute inventory plugin
+# Terraform owns the lifecycle
+resource "local_sensitive_file" "ansible_gcp_creds" {
+  content         = data.bitwarden-secrets_secret.gcp_sa_key.value
+  filename        = "${path.root}/../ansible/.gcp-sa-key.json"
+  file_permission = "0600"
+}
